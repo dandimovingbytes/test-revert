a5a314098db3928078a0db4de70812424352eda2
unknown <dandirizky@movingbytesplayground.com>
2024-07-08 10:06:37 +0700

    Revert "commit 2"
    This reverts commit f6a440cd136dd9d674f99458c7f5f039c1418024.

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const satu = 1
 const dua = 2
 const tiga = 3
 const empat = 4
+<<<<<<< HEAD
 const lima = 5
 const enam = 6
 const tujuh = 7
 const delapan = 8
 
+=======
+const lima = 5
+>>>>>>> parent of f6a440c (commit 2)
